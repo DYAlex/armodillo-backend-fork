a97f93b855a1f0b68aed5e2c2960947a9bb4be47
AnnaArmodillo <109468895+AnnaArmodillo@users.noreply.github.com>
2023-03-12 00:37:41 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import { Header } from './components/Header/Header';
 function App() {
   return (
     <div className="App">
-      <div>Header</div>
+      <Header />
       <Outlet />
-      <div>Footer</div>
+      <Footer />
     </div>
   );
 }
