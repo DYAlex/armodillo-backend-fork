338c3632173f2d68ef32ee5c06ad0c1313ed2588
Ekaterina Bulycheva <ekaterinabul21@mail.ru>
2023-03-11 17:29:58 +0300

    create Header, Main and Footer components

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,18 @@
+import styles from './Main.module.css';
+import surveyLayout from './survey1.png';
+
+export function Main() {
+  return (
+    <main>
+      <section className={styles.containerHome}>
+        <div className={styles.containerLeft}>
+          <h1>Создайте опрос в один клик</h1>
+          <button type="button">Новый опрос</button>
+        </div>
+        <div className={styles.containerRight}>
+          <img src={surveyLayout} alt="пример опроса" />
+        </div>
+      </section>
+    </main>
+  );
+}
