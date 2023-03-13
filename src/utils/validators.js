3540ce1728f77cf9c663c8f50c4fe9e3c0f6af67
Anna Denisova <abachledacurus@gmail.com>
2023-03-14 00:52:35 +0700

    Реализована отправка данных нового опроса на сервер

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -18,3 +18,24 @@ export const signUpFormValidationSchema = Yup.object().shape({
     )
     .required('Пароль является обязательным полем'),
 });
+
+export const validationScheme = Yup.object({
+  surveyTitle: Yup.string()
+    .min(2, 'Заголовок должен состоять минимум из двух символов')
+    .max(100, 'Заголовок должен содержать не более 100 символов')
+    .required('Заголовок опроса является обязательным полем'),
+  surveyType: Yup.string()
+    .required('Тип опроса является обязательным полем'),
+  options: Yup.array(
+    Yup.object({
+      optionTitle: Yup.string().required(
+        'Заголовок ответа является обязательным полем',
+      )
+        .test('not empty', (value) => value !== ''),
+      linkurl: Yup.string().url(
+        'Необходимо указать корректную ссылку на изображение',
+      ),
+      activeLink: Yup.string().url('Необходимо указать корректную ссылку для просмотра'),
+    }),
+  ),
+});
