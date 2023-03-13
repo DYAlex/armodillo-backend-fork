abe6cd3b7c4882867f93b6f413743b9104a9694c
Yvette Demidova <yvette_demidova@mail.ru>
2023-03-13 17:50:06 +0300

    Исправление cors ошибки бэка с Access to fetch at from origin 'http://localhost:3000' has been blocked by CORS policy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
 import { checkAuth } from './checkAuth.js';
 import { usersRouter } from './routers/usersRouter.js';
@@ -15,6 +16,7 @@ const PORT = 3005;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cookieParser());
