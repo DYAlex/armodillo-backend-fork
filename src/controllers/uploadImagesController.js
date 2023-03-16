2e1d02ac7545ac7c631ce5f1d6c0eb87f19150ca
Yvette Demidova <yvette_demidova@mail.ru>
2023-03-16 13:11:35 +0300

    Фикс путей после разбития на компоненнты и вынесение папки uploads из src

--- src/controllers/uploadImagesController.js
+++ src/controllers/uploadImagesController.js
@@ -5,8 +5,10 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const uploadFolderPath = __dirname.slice(0, (__dirname.indexOf('src') - 1)) + '/uploads/'
 
 async function uploadFile(req, res) {
+  try {
     try {
         // Получаем файл, который был отправлен в наше поле "image"
         const { image } = req.files;
@@ -18,24 +20,27 @@ async function uploadFile(req, res) {
         const md5ImageName = getMd5ImageName(image)
         try {
           // Перемещаем загруженный файл в нашу папку upload под новым именем, соответствующим его md5 подписи, но с прежним расширением
-          image.mv(__dirname + '../../uploads/' + md5ImageName);
+          image.mv(uploadFolderPath + md5ImageName);
           // Все прошло успешно высылаем новое имя файла
         res.status(201).send(JSON.stringify(md5ImageName));
-        } catch (error) {
-          // Если картинки не предоставлено, то выходим
-          console.log('Could not upload image', error);
-          res.sendStatus(500);
-        }
       } catch (error) {
-        console.log('No image found in upload data');
-          return res.status(400).json('Не выбран файл для загрузки');
+        // Если картинки не предоставлено, то выходим
+        console.log('Could not upload image', error);
+        res.sendStatus(500);
       }
+    } catch (error) {
+      console.log('No image found in upload data');
+      return res.status(400).json('Не выбран файл для загрузки');
+    }
+  } catch (error) {
+    return res.sendStatus(500);
+  }
 }
 
 async function getUploadedFile(req, res) {
     try {
         const filename = req.params.filename;
-        res.sendFile(path.resolve(__dirname, '../uploads', filename));
+        res.sendFile(path.resolve(uploadFolderPath, filename));
       } catch (error) {
         return res.status(404).json('Файл с таким именем не найден')
       }
