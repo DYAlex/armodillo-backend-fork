cef6a38815f7aba90c9684f38fa3f6b61023914b
DYAlex <DYAlex@users.noreply.github.com>
2023-10-25 15:29:50 +0300

    Rollback server.js

--- src/server.js
+++ src/server.js
@@ -16,11 +16,6 @@ const PORT = 32305;
 
 const app = express();
 app.use(cors());
-app.use(function(_, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cookieParser());
